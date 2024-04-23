42861309b1c14e0c06fdaee0a8d65914b3ffb9b9
c0b23160 <c0b231604f@edu.teu.ac.jp>
2024-04-23 14:31:45 +0900

    練習2:爆弾

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,12 +1,16 @@
 import os
+import random
 import sys
 import pygame as pg
 
-
 WIDTH, HEIGHT = 1600, 900
+DELTA = {  # 移動量辞書
+    pg.K_UP: (0, -5),
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0),
+}
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
-
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -14,29 +18,33 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 900, 400
+    bd_img = pg.Surface((20, 20))
+    bd_img.set_colorkey((0, 0, 0))
+    pg.draw.circle(bd_img, (255, 0, 0), (10, 10), 10)
+    bd_rct = bd_img.get_rect()
+    bd_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
+    vx, vy = +5, +5
+
     clock = pg.time.Clock()
     tmr = 0
-    DELTA = {pg.K_UP:(0, -5), pg.K_DOWN:(0, 5),  #移動量辞書
-            pg.K_LEFT:(-5, 0), pg.K_RIGHT:(5, 0)}
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
         screen.blit(bg_img, [0, 0]) 
-
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
-        for k, v in DELTA.items():  #練習1
+        for k, v in DELTA.items():
             if key_lst[k]:
-                sum_mv[0] += v[0]
-                sum_mv[1] += v[1]
+               sum_mv[0] += v[0]
+               sum_mv[1] += v[1]
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+        bd_rct.move_ip(vx, vy)       
+        screen.blit(bd_img, bd_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
-
-
 if __name__ == "__main__":
     pg.init()
     main()
